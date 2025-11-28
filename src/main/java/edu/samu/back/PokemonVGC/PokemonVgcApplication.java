cadd62b8350366a58d6ec8a081b88656189d3b19
samupm11 <samuelpereiramarante@gmail.com>
2025-11-28 13:03:47 +0100

    feat: base del proyecto

---
+++ src/main/java/edu/samu/back/PokemonVGC/PokemonVgcApplication.java
@@ -0,0 +1,13 @@
+package edu.samu.back.PokemonVGC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonVgcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonVgcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/samu/back/PokemonVGC/PokemonVgcApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.samu.back.PokemonVGC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonVgcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
